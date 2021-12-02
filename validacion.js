3269e1a0daed3d5ed9d5ed04e06068da859d35ec
abicasado <90927808+abicasado@users.noreply.github.com>
2021-12-02 10:22:07 -0300

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -1,55 +1,43 @@
-var formulario = document.getElementById('formulario');
+var formulario = document.forms['un_formulario'];
 
-window.onload = iniciar;
+window.onload = iniciar; 
 
 function iniciar() {
-  document.getElementById('send').addEventListener('click', validate, false);}
+  document.getElementById('send').addEventListener('click', validate, false); // el evento se dispara fase de captura o fase de propagacion
+}
 
-function valida_Mail() {
-  var elemento = document.getElementById('Mail');
-  if (elemento.value =='') {
-    alert("El campo debe contener un mail");
+function valida_nombre() {
+  var elemento = document.getElementById('nombre');
+  if (elemento.value == '') {
+    alert("El campo nombre no puede ser vacío");
     return FontFaceSetLoadEvent;
   } 
   return true;
 }
 
-function valida_Consulta() {
+function valida_telefono() {
   var elemento = document.getElementById('tel');
-  if (consulta.lenght < 6){
-    alert('El campo debe contener al menos 6 letras');
+  if (isNaN(elemento.value)){
+    alert('El campo teléfono debe ser numérico');
     return false;
   }
   return true;
 }
 
-function valida_fecha() {
-  var dia = document.getElementById('dia').value;
-  var mes = document.getElementById('mes').value;
-  var agno = document.getElementById('agno').value;
-
-  var fecha = new Date(agno, mes, dia);
-  if (isNaN(fecha)) {
-    alert("Los campos de la fecha son incorrectos");
-    return false;
+function valida_texto() {
+    var elemento = document.getElementById('texto');
+    if (elemento.lenght < 10) {
+      alert("El campo nombre no puede tener menos de 10 caracteres");
+      return FontFaceSetLoadEvent;
+    } 
+    return true;
   }
-  return true;
-}
-
-function valida_check() {
- var campo_check = document.getElementById('mayor');
-  if(!campo_check.checked){
-    alert('Debe ser mayor de edad');
-    return false;
-  }
-  return true;
-}
-
+  
 function validate(e){
-  if (valida_Mail()&&valida_Consulta()&&confirm('Pulse aceptar si desea enviar el formulario')){
+  if (valida_nombre()&&valida_telefono()&&valida_texto()&&confirm('Pulse aceptar si desea enviar el formulario')){
     return true;
   } else {
-     e.preventDefault();
+    e.preventDefault();
     return false;
   }
 }
