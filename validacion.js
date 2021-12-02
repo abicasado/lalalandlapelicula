32ded3438caa3cf142fa364fd1c3678941e90f21
abicasado <90927808+abicasado@users.noreply.github.com>
2021-12-02 09:34:59 -0300

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -1,21 +1,55 @@
-const formulario = Contacto.forms.formulario;
-let error = '';
+var formulario = document.getElementById('formulario');
 
-const validarConsulta = () => {
-    const consulta = formulacio.consulta.value;
-    if (consulta.lenght < 6) {
-        error += 'Debe ingresar al menos 6 caracteres en el campo Consulta \n'
-} }
+window.onload = iniciar;
 
-const validar = (e) => {
-    e.preventDefault ();
-    validarConsulta ();
-    (error) ?
-    alert ('Ocurrieron los siguientes errores: \n' + error)
-    :
-    alert ('Formulario enviado con exito!\n Opcion seleccionada: '+ formulario.opcion.value + '\n')
-    error = '';
-    formulario.reset ();
+function iniciar() {
+  document.getElementById('send').addEventListener('click', validate, false);}
+
+function valida_Mail() {
+  var elemento = document.getElementById('Mail');
+  if (elemento.value =='') {
+    alert("El campo debe contener un mail");
+    return FontFaceSetLoadEvent;
+  } 
+  return true;
 }
 
-formulario.addEventListener ("sumbit",validar);
+function valida_Consulta() {
+  var elemento = document.getElementById('tel');
+  if (consulta.lenght < 6){
+    alert('El campo debe contener al menos 6 letras');
+    return false;
+  }
+  return true;
+}
+
+function valida_fecha() {
+  var dia = document.getElementById('dia').value;
+  var mes = document.getElementById('mes').value;
+  var agno = document.getElementById('agno').value;
+
+  var fecha = new Date(agno, mes, dia);
+  if (isNaN(fecha)) {
+    alert("Los campos de la fecha son incorrectos");
+    return false;
+  }
+  return true;
+}
+
+function valida_check() {
+ var campo_check = document.getElementById('mayor');
+  if(!campo_check.checked){
+    alert('Debe ser mayor de edad');
+    return false;
+  }
+  return true;
+}
+
+function validate(e){
+  if (valida_Mail()&&valida_Consulta()&&confirm('Pulse aceptar si desea enviar el formulario')){
+    return true;
+  } else {
+     e.preventDefault();
+    return false;
+  }
+}
